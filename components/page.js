1b9002be17f1ae7d197d2ca23cb15e71002c8978
okapie <tatigreuze@yahoo.co.jp>
2020-01-05 08:31:23 +0900

    Adjust code.

--- components/page.js
+++ components/page.js
@@ -1,9 +1,8 @@
 import Link from 'next/link'
 import { connect } from 'react-redux'
-import Head from 'next/head'
 
+import Head from 'next/head'
 import Counter from './counter'
-import Clock from './clock'
 
 function Page({
   error,
@@ -21,19 +20,12 @@ function Page({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>{title}</h1>
-      <Clock lastUpdate={lastUpdate} light={light} />
       <Counter />
       <nav>
         <Link href={linkTo}>
           <a>Navigate: {NavigateTo}</a>
         </Link>
       </nav>
-      {placeholderData && (
-        <pre>
-          <code>{JSON.stringify(placeholderData, null, 2)}</code>
-        </pre>
-      )}
-      {error && <p style={{ color: 'red' }}>Error: {error.message}</p>}
     </div>
   )
 }
