171e4762a764085f8c898d2f0f581a12f3a316f9
okapie <tatigreuze@yahoo.co.jp>
2020-01-05 09:40:22 +0900

    Insert OGP tag and image.

--- components/page.js
+++ components/page.js
@@ -18,9 +18,18 @@ function Page({
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Here is OGP tag SNS link test." />
+        <meta name="keyword" content="OGP tag SNS link test." />
+        <meta property="og:url" content="https://next916-seo.okapie.now.sh/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Index Page" />
+        <meta property="og:image" content="https://next916-seo.okapie.now.sh/ogp_dummy.png" />
+        <meta name="og:description" content="Here is OGP tag SNS link test." />
+        <meta name="og:site_name" content="OGP tag SNS link test" />
       </Head>
       <h1>{title}</h1>
       <Counter />
+      <img src="/ogp_dummy.png" alt="tag image" />
       <nav>
         <Link href={linkTo}>
           <a>Navigate: {NavigateTo}</a>
